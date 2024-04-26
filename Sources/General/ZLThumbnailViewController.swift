fa770f504f77ee5b7b0d17cd646e6c2931456366
Patryk Średziński <sredzinski.patryk@gmail.com>
2024-04-26 09:26:55 +0200

    Enter Edit Photo page instantly if single photo is selected

--- Sources/General/ZLThumbnailViewController.swift
+++ Sources/General/ZLThumbnailViewController.swift
@@ -1218,20 +1218,18 @@ extension ZLThumbnailViewController: UICollectionViewDataSource, UICollectionVie
                 }
                 
                 downloadAssetIfNeed(model: model, sender: self) {
-                    if self?.shouldDirectEdit(model) == false {
-                        model.isSelected = true
-                        nav?.arrSelectedModels.append(model)
-                        block(true)
-                        
-                        config.didSelectAsset?(model.asset)
-                        self?.refreshCellIndexAndMaskView()
-                        
-                        if config.maxSelectCount == 1, !config.allowPreviewPhotos {
-                            self?.doneBtnClick()
-                        }
-                        
-                        self?.resetBottomToolBtnStatus()
+                    model.isSelected = true
+                    nav?.arrSelectedModels.append(model)
+                    block(true)
+                    
+                    config.didSelectAsset?(model.asset)
+                    self?.refreshCellIndexAndMaskView()
+                    
+                    if config.maxSelectCount == 1, !config.allowPreviewPhotos {
+                        self?.doneBtnClick()
                     }
+                    
+                    self?.resetBottomToolBtnStatus()
                 }
             } else {
                 model.isSelected = false
@@ -1336,13 +1334,11 @@ extension ZLThumbnailViewController: UICollectionViewDataSource, UICollectionVie
         
         let canEditImage = config.editAfterSelectThumbnailImage &&
             config.allowEditImage &&
-            config.maxSelectCount == 1 &&
             model.type.rawValue < ZLPhotoModel.MediaType.video.rawValue
         
         let canEditVideo = (config.editAfterSelectThumbnailImage &&
             config.allowEditVideo &&
-            model.type == .video &&
-            config.maxSelectCount == 1) ||
+            model.type == .video) ||
             (config.allowEditVideo &&
                 model.type == .video &&
                 !config.allowMixSelect &&
